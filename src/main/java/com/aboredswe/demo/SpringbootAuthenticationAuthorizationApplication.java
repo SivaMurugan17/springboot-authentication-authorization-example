ec5a1c05d58cf00e70e86e072fa482cccb6c0b81
Siva Murugan <atrssivamurugan@gmail.com>
2023-12-30 11:52:29 +0530

    Added springboot starter template

---
+++ src/main/java/com/aboredswe/demo/SpringbootAuthenticationAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.aboredswe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthenticationAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthenticationAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aboredswe/demo/SpringbootAuthenticationAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aboredswe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthenticationAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
